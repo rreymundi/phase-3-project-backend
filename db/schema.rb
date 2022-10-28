7109dad3fda28d4c36f8d51c8f4468db97014c1d
Raúl Reymundi <raul@kickstarter.com>
2022-10-28 13:12:16 -0400

    created lists table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_28_165907) do
+ActiveRecord::Schema.define(version: 2022_10_28_171105) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
