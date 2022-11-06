2066982e832ca22974be0e41c8348debfbd61c82
Raúl Reymundi <raul@kickstarter.com>
2022-11-06 09:13:01 -0500

    added my list CRUD requests to the application controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,38 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Welcome to my Phase 3 project!" }.to_json
+  end
+
+  get "/lists" do
+    lists = List.all 
+    lists.to_json
+  end
+
+  get "/lists/:id" do
+    list = List.find(params[:id])
+    list.to_json(include: :tasks)
+  end
+
+  delete '/lists/:id' do
+    list = List.find(params[:id])
+    list.destroy
+    list.to_json
+  end
+
+  post '/lists' do
+    list = List.create(
+      name: params[:name],
+    )
+    list.to_json
+  end
+
+  patch '/lists/:id' do
+    list = List.find(params[:id])
+    list.update(
+      name: params[:name]
+    )
+    list.to_json
   end
 
 end
